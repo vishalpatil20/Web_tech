90547dea4667c6672e217a8049576c3e0d451970
shashank <shashankmpatil3535@gmail.com>
2024-01-15 19:01:06 +0530

    temporary

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import Header from './components/header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-  <h1 className='bg-green-600 p-4'>react router</h1>
+
     </>
   )
 }
